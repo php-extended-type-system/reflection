9f18d3c9be22a3b922c5b2f11220b780e31693f1
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-11-06 14:14:59 +0300

    Use defer in TyphoonReflectorMemoryTest

--- tests/TyphoonReflectorMemoryTest.php
+++ tests/TyphoonReflectorMemoryTest.php
@@ -12,24 +12,20 @@ final class TyphoonReflectorMemoryTest extends TestCase
 {
     public function testItIsGarbageCollected(): void
     {
-        $gcWasEnabled = gc_enabled();
-        gc_disable();
-
-        try {
-            $reflector = TyphoonReflector::build();
-            $reflection = $reflector->reflectClass(\AppendIterator::class);
-            $weakReflector = \WeakReference::create($reflector);
-            $weakReflection = \WeakReference::create($reflection);
-
-            unset($reflection, $reflector);
-
-            // assertTrue() is used instead of assertNull() to avoid huge reflector dump in the diff
-            self::assertTrue($weakReflector->get() === null, 'Reflector is not garbage collected.');
-            self::assertTrue($weakReflection->get() === null, 'ClassReflection is not garbage collected.');
-        } finally {
-            if ($gcWasEnabled) {
-                gc_enable();
-            }
+        if (gc_enabled()) {
+            gc_disable();
+            defer($_, gc_enable(...));
         }
+
+        $reflector = TyphoonReflector::build();
+        $reflection = $reflector->reflectClass(\AppendIterator::class);
+        $weakReflector = \WeakReference::create($reflector);
+        $weakReflection = \WeakReference::create($reflection);
+
+        unset($reflection, $reflector);
+
+        // assertTrue() is used instead of assertNull() to avoid huge reflector dump in the diff
+        self::assertTrue($weakReflector->get() === null, 'Reflector is not garbage collected.');
+        self::assertTrue($weakReflection->get() === null, 'ClassReflection is not garbage collected.');
     }
 }
