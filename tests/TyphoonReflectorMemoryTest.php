4194e39623c0043c7a8ef48e3c017796b25f8275
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-11-06 14:13:44 +0300

    Remove #[RunInSeparateProcess] from TyphoonReflectorMemoryTest

--- tests/TyphoonReflectorMemoryTest.php
+++ tests/TyphoonReflectorMemoryTest.php
@@ -5,25 +5,31 @@ declare(strict_types=1);
 namespace Typhoon\Reflection;
 
 use PHPUnit\Framework\Attributes\CoversNothing;
-use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use PHPUnit\Framework\TestCase;
 
 #[CoversNothing]
 final class TyphoonReflectorMemoryTest extends TestCase
 {
-    #[RunInSeparateProcess]
     public function testItIsGarbageCollected(): void
     {
+        $gcWasEnabled = gc_enabled();
         gc_disable();
-        $reflector = TyphoonReflector::build();
-        $reflection = $reflector->reflectClass(\AppendIterator::class);
-        $weakReflector = \WeakReference::create($reflector);
-        $weakReflection = \WeakReference::create($reflection);
 
-        unset($reflection, $reflector);
+        try {
+            $reflector = TyphoonReflector::build();
+            $reflection = $reflector->reflectClass(\AppendIterator::class);
+            $weakReflector = \WeakReference::create($reflector);
+            $weakReflection = \WeakReference::create($reflection);
 
-        // assertTrue() is used instead of assertNull() to avoid huge reflector dump in the diff
-        self::assertTrue($weakReflector->get() === null, 'Reflector is not garbage collected.');
-        self::assertTrue($weakReflection->get() === null, 'ClassReflection is not garbage collected.');
+            unset($reflection, $reflector);
+
+            // assertTrue() is used instead of assertNull() to avoid huge reflector dump in the diff
+            self::assertTrue($weakReflector->get() === null, 'Reflector is not garbage collected.');
+            self::assertTrue($weakReflection->get() === null, 'ClassReflection is not garbage collected.');
+        } finally {
+            if ($gcWasEnabled) {
+                gc_enable();
+            }
+        }
     }
 }
